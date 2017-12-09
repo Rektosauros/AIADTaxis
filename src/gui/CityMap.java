62a0a6563262f4dacdb0f273db4be3a670169a02
Rektosauros <mcmigas505@gmail.com>
2017-12-09 17:53:34 +0000

    stff

---
+++ src/gui/CityMap.java
@@ -0,0 +1,58 @@
+package gui;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+public class CityMap extends JPanel {
+
+	private int city[][] ={
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}};
+	
+	private BufferedImage taxi, road, sidewalk, passenger;
+	
+	public CityMap() throws IOException {
+		loadIcons();
+	}
+
+	private void loadIcons() throws IOException{
+		this.taxi = ImageIO.read(new File("images/taxi.jpg"));
+		this.road = ImageIO.read(new File("images/road.jpg"));
+		this.sidewalk = ImageIO.read(new File("images/sidewalk.jpg"));
+		this.passenger = ImageIO.read(new File("images/passenger.jpg"));
+		
+	}
+	
+	@Override
+	public void paintComponent(Graphics graphic) {
+		super.paintComponent(g); // clean
+		Graphics2D graphics = (Graphics2D) graphic;
+		drawMap(graphics);
+	}
+
+	private void drawMap(Graphics2D graphics) {
+		// TODO Auto-generated method stub
+		
+	}
+		
+}
---
+++ src/launcher/Launcher.java
@@ -0,0 +1,20 @@
+package launcher;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import aiad20177.*;
+//import gui.Simulation;
+
+
+public class Launcher {
+	
+	public static final int N_TAXIS = 2;
+	public static final int N_PASSANGERS = 4;
+	
+	//private Simulation simulation;
+
+	public Launcher() {
+		
+	}
+}
